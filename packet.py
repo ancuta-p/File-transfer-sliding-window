da28864a3005aedd496ad05b0f6e40c3077b9967
ancuta-p <51736732+ancuta-p@users.noreply.github.com>
2019-12-12 22:58:02 +0200

    Update packet.py

--- packet.py
+++ packet.py
@@ -1,5 +1,3 @@
-
-
 def make(seq_num, data=b''):
     seq_bytes=seq_num.to_bytes(4, byteorder = 'little',signed = True)
     return seq_bytes + data
@@ -8,4 +6,10 @@ def make(seq_num, data=b''):
 def extract(packet):
     seq_num=int.from_bytes(packet[0:4],byteorder = 'little',signed = True)
     return seq_num, packet[4:]
+
+
+def isempty(packet):  # last packet
+    if packet[4:] == b'':
+        return True
+    return False
 
