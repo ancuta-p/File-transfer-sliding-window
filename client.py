5219b7c4115a6d7d490e187bc3899a524a05d0cd
ancuta-p <51736732+ancuta-p@users.noreply.github.com>
2019-12-12 23:13:22 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,31 +1,36 @@
 import socket
 import packet
+import threading
+import time
+recv_addr = (socket.gethostname(), 7780)
+c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+c_socket.bind((socket.gethostname(), 8080))
+lock = threading.Lock()
+clogfile = open("c_log.txt", 'w+')
 
-def send(c_socket,filename):
+def pkts_to_send(c_socket,filename):  # packets list
     try:
         file=open(filename,'rb')
     except IOError:
-        print("Error opening file")
+        clogfile.write("error opening file")
         return
-
-    seq_num=0
-    idx=0
-    packets=[]
+    i = 0
     while True:
         data = file.readline()
         if not data:
             break
-        packets.append(packet.make(seq_num, data))
-        seq_num += 1
+        packets.append(packet.make(i, data))
+        i += 1
+    packets.append(b'')  # last packet, empty
     num_packets = len(packets)
-    slogfile.write("Total packets: "+ str(num_packets))
+    clogfile.write("Total packets: "+ str(num_packets-1))
 
     c_socket.sendto(packets[idx],(socket.gethostname(),1234))
     file.close()
 
 
-slogfile=open("s_log.txt",'w+')
-c_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-send(c_socket,"input.txt")
+pkts_to_send("input.txt")
+#ack_thread = threading.Thread(target=receive, args=())
+#ack_thread.start()
 
 c_socket.close()
